0e45ed8e26225a102fa9f5554ea982df77493293
Jose Ramirez <a30673083@gmail.com>
2012-11-21 09:26:24 -0430

    Problems 10, 11 added. Created data folder, to avoid hard-coded data in the problems's code. data.in file added to data folder.

---
+++ problems/p10.py
@@ -0,0 +1,13 @@
+#The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+#Find the sum of all the primes below two million.
+
+import sys
+sys.path.append('../utils')
+from utils import sieve
+
+def p10():
+    return sum(sieve(2000000))
+
+print p10()
+    
