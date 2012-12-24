bf59e36b043485439333e165a9fd10d8d2c9a57c
Jose Ramirez <a30673083@gmail.com>
2012-12-24 09:49:30 -0430

    Some fixes for p114.

--- problems/p114.py
+++ problems/p114.py
@@ -1,3 +1,9 @@
+#A row measuring seven units in length has red blocks with a minimum length of
+#three units placed on it, such that any two red blocks (which are allowed to
+#be different lengths) are separated by at least one black square. There are
+#exactly seventeen ways of doing this.
+
+#How many ways can a row measuring fifty units in length be filled?
 def p114(n):
     l = [1, 1, 1, 2, 4]
     if n < 5:
