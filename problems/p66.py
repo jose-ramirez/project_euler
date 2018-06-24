5dbc617c8c5d821150abab88d419e38edf21108e
Jose Ramirez <a30673083@gmail.com>
2018-06-24 07:28:49 -0300

    Problem 66, and its respective utility script

---
+++ problems/p66.py
@@ -0,0 +1,32 @@
+# Consider quadratic Diophantine equations of the form:
+#
+# x^2 – D*y^2 = 1
+#
+# For example, when D=13, the minimal solution in x is 649^2 – 13*180^2 = 1.
+#
+# It can be assumed that there are no solutions in positive integers when D is
+# square.
+#
+# By finding minimal solutions in x for D = {2, 3, 5, 6, 7}, we obtain the
+# following:
+#
+# 3^2 – 2*2^2 = 1
+# 2^2 – 3*1^2 = 1
+# 9^2 – 5*4^2 = 1
+# 5^2 – 6*2^2 = 1
+# 8^2 – 7*3^2 = 1
+#
+# Hence, by considering minimal solutions in x for D \leq 7, the largest x is
+# obtained when D=5.
+#
+# Find the value of D ≤ 1000 in minimal solutions of x for which the largest
+# value of x is obtained.
+
+from pell import pell_min_sol, is_square
+
+def p66():
+    solutions = [(D, pell_min_sol(D)[0]) for D in range(2, 1001) if not is_square(D)]
+    sorted_solutions = sorted(solutions, key=lambda p: p[1])
+    print(sorted_solutions[-1])
+
+p66()
---
+++ problems/pell.py
@@ -0,0 +1,61 @@
+from math import sqrt
+import sys
+
+def PQa(P0, Q0, D):
+    A = [0, 1]
+    B = [1, 0]
+    G = [-P0, Q0]
+    P, Q = [P0], [Q0]
+    a0 = int((P0 + sqrt(D)) / Q0)
+
+    a = [a0]
+    A.append((a[-1] * A[-1]) + A[-2])
+    B.append((a[-1] * B[-1]) + B[-2])
+    G.append((a[-1] * G[-1]) + G[-2])
+    P.append((Q[-1] * a[-1]) - P[-1])
+    Q.append((D - (P[-1] * P[-1])) // Q[-1])
+
+    while 2 * a0 not in a:
+        a, A, B, G, P, Q = step(a, A, B, G, P, Q)
+
+    l = len(a[1:])
+
+    if l % 2 == 1:
+        for i in range(l):
+            a, A, B, G, P, Q = step(a, A, B, G, P, Q)
+
+    return a, B[2:], G[2:]
+
+def step(a, A, B, G, P, Q):
+    a.append(int((P[-1] + sqrt(D)) / Q[-1]))
+    A.append((a[-1] * A[-1]) + A[-2])
+    B.append((a[-1] * B[-1]) + B[-2])
+    G.append((a[-1] * G[-1]) + G[-2])
+    P.append((Q[-1] * a[-1]) - P[-1])
+    Q.append((D - (P[-1] * P[-1])) // Q[-1])
+    return a, A, B, G, P, Q
+
+def pell_min_sol(D):
+
+    if is_square(D):
+        return -1, -1
+
+    a, B, G = PQa(0, 1, D)
+
+    l = len(a[1:])
+
+    if l % 2 == 1:
+        return G[2 * l - 1], B[2 * l - 1]
+    else:
+        return G[l - 1], B[l - 1]
+
+def verify_pell(a, b, D):
+    return a * a - (D * b * b)
+
+def is_square(n):
+    return int(sqrt(n)) ** 2 == n
+
+for D in range(2, 10001):
+    if not is_square(D):
+        a, b = pell_min_sol(D)
+        assert(verify_pell(a, b, D) == 1)
