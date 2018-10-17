0190542b62726a30c60e21d8e3a0b81cc8a5b907
Jose Ramirez <jose@take.net>
2018-10-17 13:09:29 -0300

    Some interesting problems now working, 138 needs revison I believe

--- problems/p132.py
+++ problems/p132.py
@@ -1,24 +1,26 @@
-#A number consisting entirely of ones is called a repunit. We shall define R(k)
-#to be a repunit of length k.
+# A number consisting entirely of ones is called a repunit. We shall define R(k)
+# to be a repunit of length k.
 #
-#For example, R(10) = 1111111111 = 11 * 41 * 271 * 9091, and the sum of these prime
-#factors is 9414.
+# For example, R(10) = 1111111111 = 11 * 41 * 271 * 9091, and the sum of these prime
+# factors is 9414.
 #
-#Find the sum of the first forty prime factors of R(10^9).
+# Find the sum of the first forty prime factors of R(10^9).
 
-from utils import Utils
-u = Utils()
+from context import utils
+u = utils.Utils()
 
-primes = u.sieve(165000)
-total = 0
-count = 0
-for p in primes:
-    if u.exp_mod(10, 10 ** 9, p) == 1:
-        count += 1
-        if count <= 41:
-            total += p
-        else:
-            break
+def p132():
+    primes = u.sieve(165000)
+    total = 0
+    count = 0
+    for p in primes:
+        if u.exp_mod(10, 10 ** 9, p) == 1:
+            count += 1
+            if count <= 41:
+                total += p
+            else:
+                break
+    return total - 3
 
-print total - 3
+print(p132())
 
--- problems/p134.py
+++ problems/p134.py
@@ -1,22 +1,22 @@
-#Consider the consecutive primes p1 = 19 and p2 = 23. It can be verified that
-#1219 is the smallest number such that the last digits are formed by p1 whilst
-#also being divisible by p2.
+# Consider the consecutive primes p1 = 19 and p2 = 23. It can be verified that
+# 1219 is the smallest number such that the last digits are formed by p1 whilst
+# also being divisible by p2.
 #
-#In fact, with the exception of p1 = 3 and p2 = 5, for every pair of consecutive
-#primes, p2 > p1, there exist values of n for which the last digits are formed by
-#p1 and n is divisible by p2. Let S be the smallest of these values of n.
+# In fact, with the exception of p1 = 3 and p2 = 5, for every pair of consecutive
+# primes, p2 > p1, there exist values of n for which the last digits are formed by
+# p1 and n is divisible by p2. Let S be the smallest of these values of n.
 #
-#Find  S for every pair of consecutive primes with 5 <= p1 <= 1000000.
+# Find S for every pair of consecutive primes with 5 <= p1 <= 1000000.
 
 from utils import Utils
 u = Utils()
 
 a = u.sieve(1100000)
 
-"""
-    It is assumed that n is a power of 10.
-"""
 def phi(n):
+    """
+        It is assumed that n is a power of 10.
+    """
     return (2 * n) / 5
 
 def f1(p1, p2):
@@ -26,8 +26,8 @@ def f1(p1, p2):
     s = pow(p2, q, r)
     return ((((p1 * p2 * s) / r) % p2) * r) + p1
 
-l = [f1(a[i], a[i + 1]) for i in range(2, len(a) - 1) if a[i] < 10 ** 6]
+def p134():
+    l = [f1(a[i], a[i + 1]) for i in range(2, len(a) - 1) if a[i] < 10 ** 6]
+    return sum(l)
 
-print sum(l)
-#print l
-#print [l[i] % a[i + 3] for i in range(len(l))]
+print(p134())
